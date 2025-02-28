87f7a3e8afa4fa721722280e2be61d243de79bb5
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-02-28 20:40:23 +0100

    creation of the nevigation bar

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,3 +3,29 @@ const config = {
 };
 
 export default config;
+
+
+// import { Outfit } from "next/font/google";
+// import tailwindcss from "tailwindcss";
+
+// export default {
+//   plugins: [
+//     tailwindcss({
+//       content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+//       theme: {
+//         extend: {
+//           colors: {
+//             lightHover: "#fcf4ff",
+//             darkHover: "#2a004a",
+//             darkTheme: "11001F"
+//           },
+//           fontFamily: {
+//             Outfit: ["Outfit", "sans-serif"],
+//             Ovo: ["Ovo", "serif"]
+//           },
+//         },
+//       },
+//     }),
+   
+//   ],
+// };
