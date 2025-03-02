c2e6709130dfa9216a084206d473e733dd62e0e0
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-02 10:41:22 +0100

    added teh frmaer motion modules and aimated the header image

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,14 +1,19 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { motion } from "motion/react"
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'> 
-        <div>
+        <motion.div
+        initial={{scale: 0}}
+        whileInView={{scale: 1}}
+        transition={{duration: 0.8, type: 'spring', stiffness: 100}}
+        >
         <Image src={assets.profile_img} alt='' className='rounded-full w-32 mt-20' />
 
-        </div>
+        </motion.div>
       <h3 className='flex items-center gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Hi! I'm Eddy Steve <Image src={assets.hand_icon} alt='' className=' w-6' /></h3>
         <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo '>Full Stack Developer Base in Cameroon</h1>
         <p className='max-w-2xl mx-auto font Ovo'>
