96de87a61ea46a1c3c13604af667edd9fe0399f4
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-03-03 13:55:53 +0100

    change the iniitail color of the site to light mode

--- app/page.js
+++ app/page.js
@@ -1,49 +1,37 @@
-'use client'
-import { useState, useEffect } from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Services from "./components/Services";
-import Work from "./components/Work";
-
+'use client';
+import { useState, useEffect } from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Services from './components/Services';
+import Work from './components/Work';
 
 export default function Home() {
+  // Set default state to false (light mode)
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const [isDarkMode, setIsDarkMode] = useState(true);
-  useEffect(()=>{
-    if (
-      localStorage.getItem('theme') === 'dark' ||
-      (!localStorage.getItem('theme') &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      setIsDarkMode(true);
-    } else {
-      setIsDarkMode(false);
-    }
-    
-  },[])
-
+  // Update the DOM and localStorage when isDarkMode changes
   useEffect(() => {
     if (isDarkMode) {
       document.documentElement.classList.add('dark');
       localStorage.theme = 'dark';
     } else {
       document.documentElement.classList.remove('dark');
-      localStorage.theme = '';
+      localStorage.theme = 'light';
     }
   }, [isDarkMode]);
-  
+
   return (
     <>
-    <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-    <Header isDarkMode={isDarkMode} />
-    <About isDarkMode={isDarkMode}/>
-    <Services isDarkMode={isDarkMode} />
-    <Work isDarkMode={isDarkMode} />
-    <Contact isDarkMode={isDarkMode}/>
-    <Footer isDarkMode={isDarkMode} />
+      <Navbar isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <Header isDarkMode={isDarkMode} />
+      <About isDarkMode={isDarkMode} />
+      <Services isDarkMode={isDarkMode} />
+      <Work isDarkMode={isDarkMode} />
+      <Contact isDarkMode={isDarkMode} />
+      <Footer isDarkMode={isDarkMode} />
     </>
   );
 }
