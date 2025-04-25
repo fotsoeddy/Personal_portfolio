aa1774f22ef12a04d4169c4b86853c75a33914ff
fotsoeddy <fotsotachulaeddysteve@gmail.com>
2025-04-25 08:28:45 +0100

    use the speed of vercel and analytics

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,17 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const outfit = Outfit({
-  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
 const ovo = Ovo({
-  subsets: ["latin"], weight: ["400"]
+  subsets: ["latin"],
+  weight: ["400"],
 });
-
-
 
 export const metadata = {
   title: "Portfolio",
@@ -19,11 +20,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="scroll-smooth  ">
+    <html lang="en" className="scroll-smooth">
       <body
         className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
+        {/* Vercel Analytics */}
+        <Analytics />
+        {/* Vercel Speed Insights */}
+        <SpeedInsights />
       </body>
     </html>
   );
